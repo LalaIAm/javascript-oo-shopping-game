d7b8b317ffdd3a66127f951e119788d774c50898
LalaIAm <lthorud12@gmail.com>
2021-04-07 18:04:47 -0500

    m3

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -217,10 +217,19 @@ function loadMasterData() {
 }
 
 // Complete this function
-const findProductById = (id) => {};
+const findProductById = (id) => {
+    return (product) => {
+        if(product.id === id){
+            return true;
+        }
+        return false
+    }
+};
 
 // Complete this function
-const generateProductId = () => {};
+const generateProductId = () => {
+    return Math.floor(Math.random() * 20)
+};
 
 
 const getProduct = (prodList, pId) => {
